4e2522954cba96b79b5ae9e347a0c421276f5449
Corey Zumar <corey.zumar@databricks.com>
2019-04-04 14:58:14 -0700

    Rename

--- wandb/test_cases.py
+++ wandb/test_cases.py
@@ -3,14 +3,14 @@ import wandb
 def multiple_values_same_step():
     for i in range(3):
         val = {
-            "cat": i,
+            "my_metric": i,
         }
         wandb.log(val, step=0)
 
 
 def same_value_multiple_steps():
     val = {
-        "cat": 1,
+        "my_metric": 1,
     }
     for i in range(3):
         wandb.log(val, step=i)
@@ -18,64 +18,64 @@ def same_value_multiple_steps():
 def diff_value_multiple_steps():
     for i in range(3):
         val = {
-            "cat": i,
+            "my_metric": i,
         }
         wandb.log(val, step=i)
 
 def same_value_no_step():
     for i in range(3):
         val = {
-            "cat": 1
+            "my_metric": 1
         }
         wandb.log(val)
 
 def diff_value_no_step():
     for i in range(3):
         val = {
-            "cat": 1
+            "my_metric": 1
         }
         wandb.log(val)
 
 def large_step_numbers():
     for i in range(3):
         val = {
-            "cat": 1
+            "my_metric": 1
         }
         wandb.log(val, step=i + 50)
 
 def update_previous_step():
     for i in range(10):
         val = {
-            "cat": i
+            "my_metric": i
         }
         wandb.log(val, step=i % 3)
 
 def negative_step():
     val = {
-        "cat": 1,
+        "my_metric": 1,
     }
     wandb.log(val, step=-5)
 
 def multiple_values_different_steps_large_step_first():
     val1 = {
-        "cat": 1,
+        "my_metric": 1,
     }
     wandb.log(val1, step=7)
 
     val2 = {
-        "dog": 1,
+        "second_metric": 1,
     }
     wandb.log(val2, step=5)
 
 
 def multiple_values_different_steps_small_step_first():
     val1 = {
-        "cat": 1,
+        "my_metric": 1,
     }
     wandb.log(val1, step=5)
 
     val2 = {
-        "dog": 1,
+        "second_metric": 1,
     }
     wandb.log(val2, step=7)
 
